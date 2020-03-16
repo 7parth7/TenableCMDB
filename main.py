64cc9575033e29ceadd5c7f2f4620ce6f83c25bb
Parth Bhavsar <7parthbhavsar@gmail.com>
2020-03-16 13:10:15 -0400

    enc

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ Created on Tue Mar 10 13:22:29 2020
 import os
 import requests,json
 from tenable.sc import TenableSC
+from cryptography.fernet import Fernet
+key = Fernet.generate_key()
+print(key)
+key = key.decode()
+print(key)
+cipher_suite = Fernet(key)
+print (cipher_suite)
+
+
+
 
 USER = os.environ.get("TenableUser")
 PASS = os.environ.get("TenablePassword")
-
+encrypted = cipher_suite.encrypt(PASS.encode())
+print(encrypted)
 
 # Login to Tenable.sc
-sc = TenableSC('tenable.partners.org')
-sc.login(USER , PASS)
+# sc = TenableSC('tenable.partners.org')
+# sc.login(USER , PASS)
 
-running = sc.scans.launch(1924)
-print(running)
